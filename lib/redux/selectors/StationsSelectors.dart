2a3cc4015dda4d967b4dcb0edcb2894fa81afd02
Mattia Natali <matitalatina@gmail.com>
2018-12-02 12:30:30 +0100

    Bug fix with station without prices, update app version

--- lib/redux/selectors/StationsSelectors.dart
+++ lib/redux/selectors/StationsSelectors.dart
@@ -2,7 +2,7 @@ import 'package:gaslow_app/models/GasStation.dart';
 import 'package:gaslow_app/redux/StationsState.dart';
 
 List<GasStation> getStationsSortedByPrice(StationsState state) {
-  var stations = List<GasStation>.from(state.stations);
+  var stations = List<GasStation>.from(state.stations).where((s) => s.prices.isNotEmpty).toList();
   return stations..sort((s1, s2) => s1.prices
       .firstWhere((p) => p.isSelf && p.fuelType.contains("enz"), orElse: () => s1.prices[0])
       .price
