0718f85093b920db37cccb7079b326d84f551bfe
Mattia Natali <matitalatina@gmail.com>
2019-04-20 19:01:20 +0200

    Fix travis

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    flutter build apk --release --no-track-widget-creation;
+    flutter build apk --release;
     cd android;
     bundle install;
     bundle exec fastlane android deploy;
