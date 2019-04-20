10bcda18a498080cb486d3311c68bea9df74e49e
Mattia Natali <matitalatina@gmail.com>
2019-04-20 18:44:46 +0200

    Switch to flutter master branch

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    flutter build apk --release;
+    flutter build apk --release --no-track-widget-creation;
     cd android;
     bundle install;
     bundle exec fastlane android deploy;
