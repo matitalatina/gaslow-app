5694fe204d901af47a75ba469c750008520f9c92
Mattia Natali <matitalatina@gmail.com>
2019-07-10 20:24:31 +0200

    Aab instead of apk

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-    flutter build apk --release;
+    flutter build appbundle --target-platform android-arm,android-arm64;
     cd android;
     bundle install;
     bundle exec fastlane android deploy;
